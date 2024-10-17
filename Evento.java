5ff89b0b5ae86eda7004008c12107d1b7f79a4d1
leonardozaani <leonardo.zani24@gmail.com>
2024-10-17 20:40:39 -0300

    Ajuste em arquivo Evento.java e Palestra.java

--- Evento.java
+++ Evento.java
@@ -2,4 +2,51 @@ import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Evento {
+    protected String nome;
+    protected String data;
+    protected String local;
+    protected int capacidadeMaxima;
+    protected List<Participante> participantes;
+
+    public Evento(String nome, String data, String local, int capacidadeMaxima) {
+        this.nome = nome;
+        this.data = data;
+        this.local = local;
+        this.capacidadeMaxima = capacidadeMaxima;
+        this.participantes = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Participante> getParticipantes() {
+        return participantes;
+    }
+
+    public boolean adicionarParticipante(Participante p) {
+        if (participantes.size() >= capacidadeMaxima) {
+            System.out.println("Evento já está lotado!");
+            return false;
+        }
+        for (Participante participante : participantes) {
+            if (participante.getEmail().equals(p.getEmail())) {
+                System.out.println("Participante já cadastrado no evento!");
+                return false;
+            }
+        }
+        participantes.add(p);
+        return true;
+    }
+
+    public void listarParticipantes() {
+        for (Participante p : participantes) {
+            System.out.println(p);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return nome + " - " + data + " - Capacidade: " + capacidadeMaxima + " - Inscritos: " + participantes.size();
+    }
     }
--- Palestra.java
+++ Palestra.java
@@ -1,3 +1,29 @@
 public class Palestra extends Evento {
-    
+    private String palestrante;
+    private int duracao;
+    private String temas;
+
+    public Palestra(String nome, String data, String local, int capacidadeMaxima, String palestrante, int duracao, String temas) {
+        super(nome, data, local, capacidadeMaxima);
+        this.palestrante = palestrante;
+        this.duracao = duracao;
+        this.temas = temas;
+    }
+
+    public String getPalestrante() {
+        return palestrante;
+    }
+
+    public int getDuracao() {
+        return duracao;
+    }
+
+    public String getTemas() {
+        return temas;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + " - Palestrante: " + palestrante + " - Duração: " + duracao + " horas - Temas: " + temas;
+    }
 }
